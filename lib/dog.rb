8c3e172b6e942422421a174568e917d33a489381
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-10-30 01:29:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,20 +4,13 @@ class Dog
   end
      def sit
       puts "The Dog is sitting"
+    end
 end
 fido=Dog.new
 fido.bark
 fido.sit
 
 
-class Dog
-  def bark
-    puts "Woof!"
-  end
-end
- 
-fido = Dog.new
-fido.bark #> "Woof!"
 
 
 
